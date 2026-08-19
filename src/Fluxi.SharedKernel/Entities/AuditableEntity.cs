93eb07a5fcd4068718253340b702715797420a75
Rodrigo Vasconcelos <47873785+rrvasconcelos@users.noreply.github.com>
2026-08-18 22:18:12 -0300

    refactor: transformado em primary constructor

--- src/Fluxi.SharedKernel/Entities/AuditableEntity.cs
+++ src/Fluxi.SharedKernel/Entities/AuditableEntity.cs
@@ -1,20 +1,15 @@
 namespace Fluxi.SharedKernel.Entities;
 
-public abstract class AuditableEntity : Entity
+public abstract class AuditableEntity(Guid id) : Entity(id)
 {
-    #region Constructors
 
-    protected AuditableEntity(Guid id)
-        : base(id)
-    {
-        CreatedOn = DateTime.UtcNow;
-    }
+    #region Constructors
 
     #endregion
 
     #region Properties
 
-    public DateTime CreatedOn { get; private set; }
+    public DateTime CreatedOn { get; private set; } = DateTime.UtcNow;
 
     public DateTime? UpdatedOn { get; private set; }
 
