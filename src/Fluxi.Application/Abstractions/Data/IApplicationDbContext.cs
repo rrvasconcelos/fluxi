b9d82a85c8110f7ecce88c0d55f15ff6d02a9202
Rodrigo Vasconcelos <47873785+rrvasconcelos@users.noreply.github.com>
2026-08-25 17:09:44 -0300

    feat: adicionado camada de application ao projeto

---
+++ src/Fluxi.Application/Abstractions/Data/IApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Fluxi.Domain.Accounts.Entities;
+using Fluxi.Domain.Categories.Entities;
+using Fluxi.Domain.FixedExpenses.Entities;
+using Fluxi.Domain.Incomes.Entities;
+using Fluxi.Domain.Invoices.Entities;
+using Fluxi.Domain.Transactions.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fluxi.Application.Abstractions.Data;
+
+public interface IApplicationDbContext
+{
+    DbSet<Account> Accounts { get; }
+    DbSet<Category> Categories { get; }
+    DbSet<FixedExpense> FixedExpenses { get; }
+    DbSet<Income> Incomes { get; }
+    DbSet<Invoice> Invoices { get; }
+    DbSet<Transaction> Transactions { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ src/Fluxi.SharedKernel/Results/Result.cs
@@ -0,0 +1,116 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Fluxi.SharedKernel.Results;
+
+public class Result
+{
+    public Result(bool isSuccess, Error error)
+    {
+        if (isSuccess && error != Error.None ||
+            !isSuccess && error == Error.None)
+        {
+            throw new ArgumentException("Invalid error", nameof(error));
+        }
+
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; }
+
+    public bool IsFailure => !IsSuccess;
+
+    public Error Error { get; }
+
+    public static Result Success() => new(true, Error.None);
+
+    public static Result<TValue> Success<TValue>(TValue value) =>
+        new(value, true, Error.None);
+
+    public static Result Failure(Error error) => new(false, error);
+
+    public static Result<TValue> Failure<TValue>(Error error) =>
+        new(default, false, error);
+}
+
+public class Result<TValue> : Result
+{
+    private readonly TValue? _value;
+
+    public Result(TValue? value, bool isSuccess, Error error)
+        : base(isSuccess, error)
+    {
+        _value = value;
+    }
+
+    [NotNull]
+    public TValue Value => IsSuccess
+        ? _value!
+        : throw new InvalidOperationException("The value of a failure result can't be accessed.");
+
+    public static implicit operator Result<TValue>(TValue? value) =>
+        value is not null ? Success(value) : Failure<TValue>(Error.NullValue);
+
+    public static Result<TValue> ValidationFailure(Error error) =>
+        new(default, false, error);
+}
+
+public record Error
+{
+    public static readonly Error None = new(string.Empty, string.Empty, ErrorType.Failure);
+    public static readonly Error NullValue = new(
+        "General.Null",
+        "Null value was provided",
+        ErrorType.Failure);
+
+    public Error(string code, string description, ErrorType type)
+    {
+        Code = code;
+        Description = description;
+        Type = type;
+    }
+
+    public string Code { get; }
+
+    public string Description { get; }
+
+    public ErrorType Type { get; }
+
+    public static Error Failure(string code, string description) =>
+        new(code, description, ErrorType.Failure);
+
+    public static Error NotFound(string code, string description) =>
+        new(code, description, ErrorType.NotFound);
+
+    public static Error Problem(string code, string description) =>
+        new(code, description, ErrorType.Problem);
+
+    public static Error Conflict(string code, string description) =>
+        new(code, description, ErrorType.Conflict);
+}
+
+public enum ErrorType
+{
+    Failure = 0,
+    Validation = 1,
+    Problem = 2,
+    NotFound = 3,
+    Conflict = 4
+}
+
+public sealed record ValidationError : Error
+{
+    public ValidationError(Error[] errors)
+        : base(
+            "Validation.General",
+            "One or more validation errors occurred",
+            ErrorType.Validation)
+    {
+        Errors = errors;
+    }
+
+    public Error[] Errors { get; }
+
+    public static ValidationError FromResults(IEnumerable<Result> results) =>
+        new(results.Where(r => r.IsFailure).Select(r => r.Error).ToArray());
+}
